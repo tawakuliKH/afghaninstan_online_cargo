cbbe85e9f693ebc5fd5688528e6afd0f3c0a50e3
tawakuliKH <tawakuli456@gmail.com>
2026-07-14 22:41:49 +0330

    Update Email Settings, add real favicon.svg

--- server/src/lib/email.ts
+++ server/src/lib/email.ts
@@ -2,7 +2,7 @@ import { Resend } from 'resend'
 
 const resend = new Resend(process.env.RESEND_API_KEY)
 
-const FROM = 'Afghanistan Online Cargo <onboarding@resend.dev>'
+const FROM = 'Afghanistan Online Cargo <support@afghancargo.online>'
 
 export async function sendEmail(to: string, subject: string, html: string) {
   if (!process.env.RESEND_API_KEY) {
